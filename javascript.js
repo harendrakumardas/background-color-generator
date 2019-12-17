302bab92cb5c3dd8124dfdfd59df75714301ddea
harendrakumardas <58803075+harendrakumardas@users.noreply.github.com>
2019-12-17 19:38:51 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+var css = document.querySelector("h1");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body= document.getElementById("gradient")
+
+
+function colorchanger() {
+	body.style.background = "linear-gradient(to right,"+color1.value +","+color2.value +")";
+
+}
+color1.addEventListener("input",colorchanger)
+color2.addEventListener("input",colorchanger)
